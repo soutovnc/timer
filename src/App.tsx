8a074432b1eb16f3ec8cf14ca348d9a1906b88c5
Vinicius <viniciusdesouto@gmail.com>
2023-01-16 02:16:52 -0300

    apply principal concepts of styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from "./components/Button";
+
 export function App() {
 
   return (
-    <h1>hi</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }
