cbd97b6700ee8cd40764a6fccea965235efeca48
Vinicius <viniciusdesouto@gmail.com>
2023-01-16 02:51:44 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
@@ -12,6 +13,7 @@ export function App() {
       <Button variant="danger" />
       <Button />
 
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
