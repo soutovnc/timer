9a70458d6133967791a5b8ea4acfbe3a0ff76228
Vinicius <viniciusdesouto@gmail.com>
2023-08-18 01:13:18 -0300

    make input better with additional settings

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="make cookies" />
+            <option value="project 01" />
+            <option value="banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            // make input better
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
